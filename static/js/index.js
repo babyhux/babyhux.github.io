d731af9ab77f88a05567bae46ad8db1f8f547bc9
babyhux <babyhux@gmail.com>
2020-04-01 11:01:22 +0800

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -53,6 +53,7 @@ let soul = [
 
 function randomSentence() {
   document.getElementById('sentence').innerHTML = soul[Math.floor(Math.random() * soul.length)];
+  document.getElementById('date').innerHTML = Date();
 }
 
 randomSentence();
